85f3891589c2448b716a9ce4efc37e8ca1b4339e
jv88899 <jv88899@gmail.com>
2021-09-03 12:57:15 -0500

    2 files modified

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -1,5 +1,26 @@
 import React from 'react';
+import { Typography, Button, ButtonGroup, Container } from '@material-ui/core/';
 
 export default function Create() {
-  return <div>Create page</div>;
+  return (
+    <Container>
+      <Typography
+        variant="h6"
+        component="h2"
+        gutterBottom
+        color="textSecondary"
+      >
+        Create a New Note
+      </Typography>
+
+      <Button
+        onClick={() => console.log('You clicked me')}
+        type="submit"
+        color="secondary"
+        variant="contained"
+      >
+        Submit
+      </Button>
+    </Container>
+  );
 }
