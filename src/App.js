a9f244cc6753d2812322c3456dc1b72e32ff1a7b
jv88899 <jv88899@gmail.com>
2021-09-03 14:45:17 -0500

    1 file modified

--- src/App.js
+++ src/App.js
@@ -2,19 +2,32 @@ import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Create from './pages/Create';
 import Notes from './pages/Notes';
+import { createTheme, ThemeProvider } from '@material-ui/core';
+import { purple } from '@material-ui/core/colors';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#fefefe'
+    },
+    secondary: purple
+  }
+});
 
 function App() {
   return (
-    <Router>
-      <Switch>
-        <Route exact path="/">
-          <Notes />
-        </Route>
-        <Route path="/create">
-          <Create />
-        </Route>
-      </Switch>
-    </Router>
+    <ThemeProvider theme={theme}>
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <Notes />
+          </Route>
+          <Route path="/create">
+            <Create />
+          </Route>
+        </Switch>
+      </Router>
+    </ThemeProvider>
   );
 }
 
